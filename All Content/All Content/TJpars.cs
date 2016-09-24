23eaf5930dae6b42c75bf2bde690e052d0007268
AJereli <drefaz@gmail.com>
2016-09-24 16:03:25 +0300

    TJ from Nik

--- All Content/All Content/TJpars.cs
+++ All Content/All Content/TJpars.cs
@@ -21,25 +21,32 @@ namespace All_Content
 {
     class TJpars
     {
-       
+
         HtmlParser parser = new HtmlParser();
         static string link = "https://tjournal.ru";
-
+        ContentUnit cont;
         static IConfiguration config = Configuration.Default.WithDefaultLoader();
         public TJpars()
         {
-                    
 
+            cont = new ContentUnit();
             IDocument document = BrowsingContext.New(config).OpenAsync(link).Result;
-            foreach(IElement element in  document.QuerySelector("div.l-container > div.b-container")
-                .QuerySelector("main.b-content >  div.b-w-feed > div.hereIsLoadMoreContainer")
-                .QuerySelector("div.b-block > div.b-articles loadMoreHere")
-                .QuerySelectorAll("b-articles__b b-articles__b_t2 b-articles__b_t2_1 b-articles__b_t2_1_1 jk-navigation"))
+            foreach (IElement element in document.Body.QuerySelector("div.l-container > div.b-container")
+                .QuerySelectorAll("main.b-content > div.b-w-feed > div.hereIsLoadMoreContainer > div.b-block > div.b-articles.loadMoreHere > div.b-articles__b.b-articles__b_t2.b-articles__b_t2_1.b-articles__b_t2_1_1.jk-navigation")
+
+                )
             {
-             
+                //MessageBox.Show(element.QuerySelector("div.b-articles__b__text p").TextContent);
+                cont.header = element.QuerySelector("div.b-articles__b__title").TextContent;
+                cont.description = element.QuerySelector("div.b-articles__b__text p").TextContent;
+                cont.imgUrl = element.QuerySelector("div.b-articles__b__image img").GetAttribute("src");
+                cont.URL = element.QuerySelector("div.b-articles__b__content div.b-articles__b__image a").GetAttribute("href");
+                cont.tags = "TJ";
+                cont.source = link;
+                cont.LoadContentToSQL();
             }
-        
+
         }
-       
+
     }
 }
