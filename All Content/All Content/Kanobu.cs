0283c8fb835616e8cac938566c7de98d0a1b175a
AJereli <drefaz@gmail.com>
2016-09-24 16:00:05 +0300

    Kanobu

---
+++ All Content/All Content/Kanobu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+using AngleSharp.Dom;
+
+namespace All_Content
+{
+    class Kanobu : SiteForPars
+    {
+       
+
+        public Kanobu()
+        {
+
+        }
+    }
+}
