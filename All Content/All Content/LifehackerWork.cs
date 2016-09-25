f27bd98f30879f3f986548b5bb58f9be17074710
Mr-revi4 <mr_revi4@mail.ru>
2016-09-25 12:56:11 +0300

    LifehackerParse

---
+++ All Content/All Content/LifehackerWork.cs
@@ -0,0 +1,22 @@
+﻿using AngleSharp.Dom;
+//Лайфхакер - Работа
+namespace All_Content
+{
+    class LifehackerWork : SiteForPars
+    {
+        public LifehackerWork() : base("https://lifehacker.ru/topics/work/")
+        {
+            foreach (IElement element in document.QuerySelector("div.flow.container-fluid div.row div.col-md-18.flow__posts")
+                .QuerySelectorAll("div.flow-post"))
+            {
+                cu.URL = element.QuerySelector("div.col-sm-16 a").GetAttribute("href");
+                cu.header = element.QuerySelector("div.col-sm-16 a h2.flow-post__title").TextContent;
+                cu.source = link;
+                cu.description = element.QuerySelector("div.col-sm-16 p.flow-post__excerpt").TextContent;
+                cu.imgUrl = element.QuerySelector("img.flow-post__image").GetAttribute("src");
+                cu.tags = "Лайфхакер, Работа";
+                cu.LoadContentToSQL();
+            }
+        }
+    }
+}
